f92663d22ac63288af79c3cbc95ed28f5d32a0ba
Boyz444 <ballin.alex@ya.ru>
2026-03-30 23:23:33 +0300

    Initial Commit

---
+++ MovieTracker/MovieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackerApp.swift
+//  MovieTracker
+//
+//  Created by Александр Малахов on 30.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
